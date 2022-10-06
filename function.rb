e6f7dabb195c66c436c91cebf93ba20cee074661
zephyrz73 <xzhou1551@outlook.com>
2022-10-06 03:20:40 -0700

    get method

--- function.rb
+++ function.rb
@@ -3,11 +3,15 @@
 require 'json'
 require 'jwt'
 require 'pp'
+require 'bundler/setup' #to do remove
 
 def main(event:, context:)
   # You shouldn't need to use context, but its fields are explained here:
   # https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-  response(body: event, status: 200)
+  if event['path'] == '/' && event['httpMethod'] == 'GET'
+    get(body: event)
+  end
+  #response(body: event, status: 200)
 end
 
 def response(body: nil, status: 200)
@@ -15,6 +19,25 @@ def response(body: nil, status: 200)
     body: body ? body.to_json + "\n" : '',
     statusCode: status
   }
+end
+
+def get(body: nil)
+  auth = body['headers']['Authorization']
+  if auth == nil
+    response(body: {'error': 'please specify auth token'}, status: 403)
+  else
+    encoded_token = auth[7..-1]
+    token = JWT.decode encoded_token, ENV['JWT_SECRET'], 'HS256'
+    if token[0]['exp'].to_i < Time.now.to_i
+      response(body: {'error': 'token expired'}, status: 401)
+    else
+      if token[0]['nbf'].to_i > Time.now.to_i
+        response(body: {'error': 'token not yet valid'}, status: 401)
+      else
+        response(body: token[0]['data'], status: 200)
+      end
+    end
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
