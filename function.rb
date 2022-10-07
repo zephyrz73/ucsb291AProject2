904ecf8f9e317b577bd9a3c2a48110a2331dbe79
zephyrz73 <xzhou1551@outlook.com>
2022-10-06 21:43:03 -0700

    parse

--- function.rb
+++ function.rb
@@ -14,10 +14,10 @@ def main(event:, context:)
     elsif event['path'] == '/token' && event['httpMethod'] == 'POST'
       post(body: event)
     else
-      response(body: {'error': 'wrong http method'}, status: 405)
+      response(body: {"error": "wrong http method"}, status: 405)
     end
   else
-    response(body: {'error': 'resource not found'}, status: 404)
+    response(body: {"error": "resource not found"}, status: 404)
   end
   #response(body: event, status: 200)
 end
@@ -33,20 +33,20 @@ def get(body: nil)
   auth_wording = body['headers'].keys.grep(/#{"authorization"}/i)
   auth = body['headers'][auth_wording[0]]
   if auth == nil
-    response(body: {'error': 'please specify auth token'}, status: 403)
+    response(body: {"error": "please specify auth token"}, status: 403)
   else
     begin
       encoded_token = auth.split(" ")[1]
       token = JWT.decode encoded_token, ENV['JWT_SECRET'], 'HS256'
-      if token[0]['exp'].to_i < Time.now.to_i
-        response(body: {'error': 'token expired'}, status: 401)
-      elsif token[0]['nbf'].to_i > Time.now.to_i
-        response(body: {'error': 'token not yet valid'}, status: 401)
+      if token[0]["exp"].to_i < Time.now.to_i
+        response(body: {"error": "token expired"}, status: 401)
+      elsif token[0]["nbf"].to_i > Time.now.to_i
+        response(body: {"error": "token not yet valid"}, status: 401)
       else
         response(body: token[0]["data"], status: 200)
       end
     rescue JWT::DecodeError
-      response(body: {'error': 'decode error'}, status: 403)
+      response(body: {"error": "decode error"}, status: 403)
     end
   end
 end
@@ -60,7 +60,7 @@ def post(body: nil)
     response(body: {'error': 'not a valid json'}, status: 422)
   else
     uncoded_token = {
-      data: body['body'],
+      data: JSON.parse(body["body"]),
       exp: Time.now.to_i + 5,
       nbf: Time.now.to_i + 2
     }
