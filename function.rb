86ae6f965d02fcfa34d8d4f12fb08c5626a64ff3
zephyrz73 <xzhou1551@outlook.com>
2022-10-06 20:29:37 -0700

    response body is not json

--- function.rb
+++ function.rb
@@ -24,7 +24,7 @@ end
 
 def response(body: nil, status: 200)
   {
-    body: body ? body.to_json + "\n" : '',
+    body: body,
     statusCode: status
   }
 end
@@ -43,7 +43,7 @@ def get(body: nil)
       elsif token[0]['nbf'].to_i > Time.now.to_i
         response(body: {'error': 'token not yet valid'}, status: 401)
       else
-        response(body: token[0]['data'].to_json, status: 200)
+        response(body: token[0]['data'], status: 200)
       end
     rescue JWT::DecodeError
       response(body: {'error': 'decode error'}, status: 403)
